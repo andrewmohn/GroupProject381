8d0c701969277b09c34182a8e87e58ebcf251d7b
andrewmohn <amohn@umich.edu>
2017-04-17 23:41:07 -0400

    Revert "Reset to last build"
    This reverts commit 019cd13ceaf97a2b6fc28e5430039a30c7884f6a.

--- ColorProcessing/ColorProcessing/OutputAdapter.cpp
+++ ColorProcessing/ColorProcessing/OutputAdapter.cpp
@@ -9,19 +9,11 @@ bool OutputAdapter::requestColor() //This function will be activated by the robo
 	return true;
 }
 void OutputAdapter::outputNumber(LegoColor color)
-<<<<<<< HEAD
 {
 	std::cout << "Outputting color " << color << " with pin output: ";
 	int num1 = color & 1;
 	int num2 = (color & 2) >> 1;
 	int num3 = (color & 4) >> 2;
-=======
-{ 
-	std::string num = { 40 };
-<<<<<<< HEAD
->>>>>>> ad36c28... commit
-=======
->>>>>>> ad36c28... commit
 
 	if (num1 == 1)
 	{
@@ -47,5 +39,7 @@ void OutputAdapter::outputNumber(LegoColor color)
 	{
 		std::cout << "0";
 	}
+
+	std::cout << std::endl;
 	return;
 }
